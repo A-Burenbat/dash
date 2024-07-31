c1168788c6ae404e64cf9db6b77d438cb40c7ec3
A-Burenbat <developersmongolia@gmail.com>
2024-07-31 22:49:23 +0800

    nemdeg bolov

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,14 +4,17 @@ import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from 'react';
 import Table from "@/app/ui/invoices/table";
+import { fetchInvoicesPages } from "@/app/lib/data";
+import Pagination from "@/app/ui/invoices/pagination";
 
-export default function Page({
+export default async function Page({
     searchParams,
 }: {
     searchParams?: {query?: string; page?: string};
 }){
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -26,7 +29,7 @@ export default function Page({
                     <Table query={query} currentPage={currentPage} />
                 </Suspense>
                 <div className="mt-5 flex w-full justify-center">
-                    {/* <Pagination totalPages={totalPages} /> */}
+                    <Pagination totalPages={totalPages} />
                 </div>
             </div>
         </div>
